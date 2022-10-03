06ca49c51c06e36c32bd339ef74f47ff7d0a24c8
dap5844 <57582697+dap5844@users.noreply.github.com>
2022-10-03 15:03:15 -0700

    Add files via upload

--- lucky_sum_login.py
+++ lucky_sum_login.py
@@ -33,21 +33,19 @@ def lucky_sum(a,b):
     integ_2 = b
     sum = 0
     k = 0
-    if integ_2 > integ_1:
+    if integ_2 > integ_1: #this checks if b is greater than a
         i = integ_1
         for i in range(integ_1,integ_2):
-            if i%3 != 0:
+            if i%3 != 0: #check if the number in the for loop is not divisible by 3 to add it
                 k = i
-                #print(k)
                 sum += k
     elif integ_2 < integ_1:
         i = integ_2
         for i in range(integ_2,integ_1):
-            if i%3 != 0:
+            if i%3 != 0: #check if the number in the for loop is not divisible by 3 to add it
                 k = i
-                #print(k)
                 sum += k
-    if integ_2%3 != 0:
+    if integ_2%3 != 0: #check if the 2nd integer is not divisible by 3 to add it to the sum
         SUM = sum + integ_2
     else:
         SUM = sum
@@ -55,10 +53,10 @@ def lucky_sum(a,b):
         
 def main():
 
-    integ_1 = int(input("Enter the first integer: "))
-    integ_2 = int(input("Enter the second integer: "))
-    SUM = lucky_sum(integ_1,integ_2)
-    print(f"The sum of the lucky numbers is {SUM:,.0f}.")
+    integ_1 = int(input("Enter the first integer: ")) #ask user for 1st integer
+    integ_2 = int(input("Enter the second integer: ")) #ask user for 1st integer
+    SUM = lucky_sum(integ_1,integ_2) #run lucky sum function
+    print(f"The sum of the lucky numbers is {SUM:,.0f}.") #print summed value
     
 """Do not change anything below this line."""
 if __name__ == "__main__":
