26e1cbf45467d4de9a6458bef3c1c6033da07c30
dap5844 <57582697+dap5844@users.noreply.github.com>
2022-10-03 14:55:38 -0700

    Add files via upload

---
+++ lucky_sum_login.py
@@ -0,0 +1,66 @@
+"""
+Author: Daniel Perez, perez294@purdue.edu
+Assignment: 04.2 - Lucky Sum
+Date: 10/3/2022
+Description:
+    This program asks the user for two integers and calculates the sum of all numbers within the two integers after removing values divisible by 3.
+Contributors:
+    None, Stackflow
+My contributor(s) helped me:
+    [ ] understand the assignment expectations without
+        telling me how they will approach it.
+    [ ] understand different ways to think about a solution
+        without helping me plan my solution.
+    [ ] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor.
+Academic Integrity Statement:
+    I have not used source code obtained from any unauthorized
+    source, either modified or unmodified; nor have I provided
+    another student access to my code.  The project I am
+    submitting is my own original work.
+"""
+
+"""Import additional modules below this line (starting with unit 6)."""
+
+
+
+"""Write new functions below this line (starting with unit 4)."""
+
+def lucky_sum(a,b):
+    integ_1 = a
+    integ_2 = b
+    sum = 0
+    k = 0
+    if integ_2 > integ_1:
+        i = integ_1
+        for i in range(integ_1,integ_2):
+            if i%3 != 0:
+                k = i
+                print(k)
+                sum += k
+    elif integ_2 < integ_1:
+        i = integ_2
+        for i in range(integ_2,integ_1):
+            if i%3 != 0:
+                k = i
+                print(k)
+                sum += k
+    return sum
+        
+def main():
+
+    integ_1 = int(input("Enter the first integer: "))
+    integ_2 = int(input("Enter the second integer: "))
+    sum = lucky_sum(integ_1,integ_2)
+    if integ_2%3 != 0:
+        SUM = sum + integ_2
+    else:
+        SUM = sum
+    print(f"The sum of the lucky numbers is {SUM:,.0f}.")
+    
+"""Do not change anything below this line."""
+if __name__ == "__main__":
+    main()
+
